b1f8b8962dcac5e8ec70c8253ef81bbdfecbc377
Elcio Yoshiaki Sato <elciosato@gmail.com>
2022-10-16 19:56:49 +0200

    Add documentation using swagger api

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from "express";
+import swaggerUI from "swagger-ui-express";
 
 import { routes } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use(routes);
 
